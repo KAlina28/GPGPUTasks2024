bc0db6005988eb60b0cf2036c33635008c509a63
KAlina28 <krylovaalina28@yandex.ru>
2025-03-24 12:30:13 +0300

    lab07

--- src/cl/prefix_sum.cl
+++ src/cl/prefix_sum.cl
@@ -1,1 +1,7 @@
 // TODO
+__kernel void prefix_sum(__global unsigned int* a, __global unsigned int* b,  int s, int n, int step, int jump) {
+    int cur_id = s + get_global_id(0) * step;
+    if (cur_id < n && cur_id >= 0) {
+        b[cur_id] = a[cur_id] + ((cur_id >= jump) ? a[cur_id - jump] : 0);
+    }
+}
--- src/main_prefix_sum.cpp
+++ src/main_prefix_sum.cpp
@@ -1,8 +1,8 @@
 #include <libgpu/context.h>
 #include <libgpu/shared_device_buffer.h>
+#include <libutils/fast_random.h>
 #include <libutils/misc.h>
 #include <libutils/timer.h>
-#include <libutils/fast_random.h>
 
 // Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:18
 #include "cl/prefix_sum_cl.h"
@@ -13,18 +13,16 @@ const int benchmarkingItersCPU = 10;
 const unsigned int max_n = (1 << 24);
 
 template<typename T>
-void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
-{
-	if (a != b) {
-		std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
-		throw std::runtime_error(message);
-	}
+void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
+    if (a != b) {
+        std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
+        throw std::runtime_error(message);
+    }
 }
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
-std::vector<unsigned int> computeCPU(const std::vector<unsigned int> &as)
-{
+std::vector<unsigned int> computeCPU(const std::vector<unsigned int> &as) {
     const unsigned int n = as.size();
 
     std::vector<unsigned int> bs(n);
@@ -33,7 +31,7 @@ std::vector<unsigned int> computeCPU(const std::vector<unsigned int> &as)
         for (int i = 0; i < n; ++i) {
             bs[i] = as[i];
             if (i) {
-                bs[i] += bs[i-1];
+                bs[i] += bs[i - 1];
             }
         }
         t.nextLap();
@@ -45,37 +43,54 @@ std::vector<unsigned int> computeCPU(const std::vector<unsigned int> &as)
     return bs;
 }
 
-int main(int argc, char **argv)
-{
-	for (unsigned int n = 4096; n <= max_n; n *= 4) {
-		std::cout << "______________________________________________" << std::endl;
-		unsigned int values_range = std::min<unsigned int>(1023, std::numeric_limits<int>::max() / n);
-		std::cout << "n=" << n << " values in range: [" << 0 << "; " << values_range << "]" << std::endl;
+int main(int argc, char **argv) {
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
 
-		std::vector<unsigned int> as(n, 0);
-		FastRandom r(n);
-		for (int i = 0; i < n; ++i) {
-			as[i] = r.next(0, values_range);
-		}
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+
+    ocl::Kernel prefix_sum(prefix_sum_kernel, prefix_sum_kernel_length, "prefix_sum");
+    prefix_sum.compile();
+
+    gpu::gpu_mem_32u as_gpu, bs_gpu;
+
+    for (unsigned int n = 4096; n <= max_n; n *= 4) {
+        as_gpu.resizeN(n);
+        bs_gpu.resizeN(n);
+        std::cout << "______________________________________________" << std::endl;
+        unsigned int values_range = std::min<unsigned int>(1023, std::numeric_limits<int>::max() / n);
+        std::cout << "n=" << n << " values in range: [" << 0 << "; " << values_range << "]" << std::endl;
+
+        std::vector<unsigned int> as(n, 0);
+        FastRandom r(n);
+        for (int i = 0; i < n; ++i) {
+            as[i] = r.next(0, values_range);
+        }
 
         const std::vector<unsigned int> cpu_reference = computeCPU(as);
 
 // prefix sum
-#if 0
+#if 1
         {
             std::vector<unsigned int> res(n);
 
             timer t;
             for (int iter = 0; iter < benchmarkingIters; ++iter) {
                 // TODO
+                as_gpu.writeN(as.data(), n);
                 t.restart();
                 // TODO
+                for (int i = 1; i < n; i *= 2) {
+                    prefix_sum.exec(gpu::WorkSize(64, n), as_gpu, bs_gpu, 0, n, 1, i);
+                    std::swap(as_gpu, bs_gpu);
+                }
                 t.nextLap();
             }
 
             std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
             std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
-
+            as_gpu.readN(res.data(), n);
             for (int i = 0; i < n; ++i) {
                 EXPECT_THE_SAME(cpu_reference[i], res[i], "GPU result should be consistent!");
             }
@@ -83,25 +98,33 @@ int main(int argc, char **argv)
 #endif
 
 // work-efficient prefix sum
-#if 0
+#if 1
         {
             std::vector<unsigned int> res(n);
 
             timer t;
             for (int iter = 0; iter < benchmarkingIters; ++iter) {
                 // TODO
+                as_gpu.writeN(as.data(), n);
                 t.restart();
                 // TODO
+                int i;
+                for (i = 1; i < n; i *= 2) {
+                    prefix_sum.exec(gpu::WorkSize(64, n / (2 * i)), as_gpu, as_gpu, (i << 1) - 1, n, 2 * i, i);
+                }
+                for (i >>= 2; i > 0; i /= 2) {
+                    prefix_sum.exec(gpu::WorkSize(64, n / (2 * i) - 1), as_gpu, as_gpu, n - i - 1, n, (-2) * i, i);
+                }
                 t.nextLap();
             }
 
             std::cout << "GPU [work-efficient]: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
             std::cout << "GPU [work-efficient]: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
-
+            as_gpu.readN(res.data(), n);
             for (int i = 0; i < n; ++i) {
                 EXPECT_THE_SAME(cpu_reference[i], res[i], "GPU result should be consistent!");
             }
         }
 #endif
-	}
+    }
 }
